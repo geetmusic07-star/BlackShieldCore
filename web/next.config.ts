91eec484ebfc32aa0706c54511f7f202aa092e8c
geetmusic07-star <geetmusic07@gmail.com>
2026-05-01 02:31:23 +0530

    Delete web directory

--- web/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Allow LAN IPs and 127.0.0.1 to talk to dev resources (HMR, RSC)
-  allowedDevOrigins: ["127.0.0.1", "192.168.31.143"],
-};
-
-export default nextConfig;
